0ce79f119e2cfb5c79310bac1957b12ad9e953eb
rnrl1215 <33127270+rnrl1215@users.noreply.github.com>
2021-05-15 15:03:51 +0900

    Create string_compression.cpp

---
+++ programmers/exercise/string_compression.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s){
+
+    int answer = 0;
+    int findCount = 0;
+    int index = 0;
+    int minCount = 987654321;
+    string findString="";
+    string compressString = "";
+
+    for(int i =1; i <= s.size(); i++)
+    {
+        findCount = i;
+        findString = s.substr(0,findCount);
+        int matchCount = 0;
+        for(int j = 0; j< s.size(); j+=findCount){
+            string tmpString = s.substr(j,findCount);
+            if( findString ==  tmpString ){
+                matchCount++;
+            } else {
+                if( matchCount>1){
+                    compressString += to_string(matchCount)+findString;
+                } else {
+                    compressString += findString;
+                }
+                findString = tmpString;
+                j-=findCount;
+                matchCount = 0;
+            }
+        }
+
+        if( matchCount>1){
+            compressString += to_string(matchCount)+findString;
+        } else{
+            compressString+=findString;
+        }
+        
+        if( compressString.size() < minCount ){
+            minCount = compressString.size();
+        }
+        compressString="";
+    }
+    answer = minCount;
+    return answer;
+}
